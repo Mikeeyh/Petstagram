963235af6a6d831de3712914728e5f8baee59100
Mikeeyh <133797823+Mikeeyh@users.noreply.github.com>
2024-02-10 00:55:40 +0200

    add

--- petstagram/pets/urls.py
+++ petstagram/pets/urls.py
@@ -1,3 +1,16 @@
-urlpatterns = (
+from django.urls import path, include
 
+from petstagram.pets.views import create_pet, delete_pet, edit_pet, details_pet
+
+urlpatterns = (
+    path("create/", create_pet, name="create pet"),
+
+    path(
+        "<str:username>/pet/<slug:pet_slug>/",
+        include([
+            path("", details_pet, name="details pet"),
+            path("edit/", edit_pet, name="edit pet"),
+            path("delete/", delete_pet, name="delete pet"),
+        ]),
+    ),
 )
